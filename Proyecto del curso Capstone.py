0a71b5b9cc3831908087c61722e33f20b9186bfc
Yo-da23 <77838449+Yo-da23@users.noreply.github.com>
2021-03-05 13:17:37 +0100

    Add files via upload

---
+++ Proyecto del curso Capstone.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Curso Capstone Coursera
+
+# ### Este notebook se utilizará para el proyecto final
+
+# #### Semana 1:
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[4]:
+
+
+print('¡Hola, Proyecto Final del Curso!')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
